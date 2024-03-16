3bd40c977b27b4c45b146c1e69c6f23ae29e8610
VladislavT <vt@burno.tech>
2024-03-16 14:14:42 +0300

    Fix issue #1

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,39 @@
 import { fileURLToPath } from 'node:url'
-import { defineConfig } from 'vite'
 
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            injectRegister: 'auto',
+            manifest: {
+                name: 'ToDo GLR',
+                short_name: 'ToDo',
+                description: 'A simple app for managing your tasks',
+                theme_color: '#ffffff',
+                icons: [
+                    // {
+                    //     src: 'pwa-192x192.png',
+                    //     sizes: '192x192',
+                    //     type: 'image/png'
+                    // },
+                    // {
+                    //     src: 'pwa-512x512.png',
+                    //     sizes: '512x512',
+                    //     type: 'image/png'
+                    // }
+                ]
+            },
+            devOptions: {
+                enabled: true
+            }
+        })
+    ],
     resolve: {
         alias: [
             {
