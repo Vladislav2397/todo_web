07b4dd7ab9240ecfb344e9131664ad0144a8c4db
VladislavT <vt@burno.tech>
2024-03-16 14:25:28 +0300

    Add env for project

--- vite.config.ts
+++ vite.config.ts
@@ -29,9 +29,9 @@ export default defineConfig({
                     // }
                 ]
             },
-            devOptions: {
-                enabled: true
-            }
+            // devOptions: {
+            //     enabled: true
+            // }
         })
     ],
     resolve: {
